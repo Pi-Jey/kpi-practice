552f7d684438a6f7bdf8954d1ccfa6623031fcfb
Pi-Jey <62254896+Pi-Jey@users.noreply.github.com>
2021-04-10 23:50:57 +0300

    Add files via upload

---
+++ Cafe.Dao/CafeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cafe.Dao.Cafe;
+
+namespace Cafe.Dao
+{
+    public class CafeDbContext: DbContext
+    {
+        public CafeDbContext(DbContextOptions<CafeDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CafeDto> Cafes { get; set; }
+    }
+}
