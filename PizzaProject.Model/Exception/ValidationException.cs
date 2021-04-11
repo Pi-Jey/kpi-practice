9ea0180b06639f8fac7a9667b2e854a018a205a1
Pi-Jey <62254896+Pi-Jey@users.noreply.github.com>
2021-04-11 14:17:43 +0300

    Add files via upload

---
+++ PizzaProject.Model/Exception/ValidationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaProject.Model.Exception
+{
+    public class ValidationException : SystemException
+    {
+        public ValidationException(string message = "") : base(message)
+        {
+
+        }
+    }
+}
