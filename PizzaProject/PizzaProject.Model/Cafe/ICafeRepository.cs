ac4567ed4a263621382a52c0d5c15721a383ae46
Pi-Jey <p.mixer2002@gmail.com>
2021-04-17 16:07:45 +0300

    init api

---
+++ PizzaProject/PizzaProject.Model/Cafe/ICafeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaProject.Model.Cafe
+{
+    public interface ICafeRepository
+    {
+        Task<List<Cafe>> GetAsync();
+        Task<Cafe> GetByIdAsync(int id);
+        Task<Cafe> UpdateNameAsync(Cafe cafe);
+        Task RemoveAsync(int id);
+        Task<Cafe> AddAsync(Cafe cafe);
+    }
+}
